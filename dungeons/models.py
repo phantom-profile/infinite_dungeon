50b2c85a15c84f3f99ca6cd5e282a91fe632cd3e
Rumyantsev Andrey <70966785+phantom-profile@users.noreply.github.com>
2024-06-02 21:15:12 +0500

    update model

--- dungeons/models.py
+++ dungeons/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Adventurer(models.Model):
+    objects = models.QuerySet
+
     name = models.CharField(blank=False, null=False, max_length=200)
-    uid = models.CharField(blank=False, null=False, max_length=100)
+    uid = models.CharField(blank=False, null=False, max_length=100, unique=True)
     level = models.IntegerField(default=1, null=False)
